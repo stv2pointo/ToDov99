e4033b81deca48af99e5e90f6c39a811f1c98d78
Steven <stvjulien@gmail.com>
2017-01-23 19:11:46 -0700

    wrote models

---
+++ ToDoListV99/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoListV99.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public virtual ICollection<List> Lists { get; set; }
+    }
+}
---
+++ ToDoListV99/Models/List.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoListV99.Models
+{
+    public class List
+    {
+        public int ListId { get; set; }
+        public string ListName { get; set; }
+        public virtual ICollection<Item> Items { get; set; }
+        public virtual ICollection<Category> Categories { get; set; }
+    }
+}
