c8ebad5b6c98acbe77006dffeb1f0b61e771fe2f
isuddreth <iansuddreth@gmail.com>
2017-01-25 19:22:07 -0700

    Revert "Add items"

--- ToDoListV99/Models/MyDbContext.cs
+++ ToDoListV99/Models/MyDbContext.cs
@@ -21,9 +21,6 @@ namespace ToDoListV99.Models
 
         public System.Data.Entity.DbSet<ToDoListV99.Models.List> Lists { get; set; }
 
-        public System.Data.Entity.DbSet<ToDoListV99.Models.Item> Items { get; set; }
-
         public System.Data.Entity.DbSet<ToDoListV99.Models.Category> Categories { get; set; }
-
     }
 }
