cca19c7ab6b3f4e8d5fcf27e18ce769b2dc5a96a
isuddreth <iansuddreth@gmail.com>
2017-01-30 17:26:43 -0700

    Revert "Add text box to items into develop" (#17)

--- ToDoListV99/Models/List.cs
+++ ToDoListV99/Models/List.cs
@@ -10,6 +10,6 @@ namespace ToDoListV99.Models
         public int ListId { get; set; }
         public string ListName { get; set; }
         public virtual ICollection<Item> Items { get; set; }
-        public virtual ICollection<ListToCategory> ListsToCategories { get; set; }
+        public virtual ICollection<Category> Categories { get; set; }
     }
 }
