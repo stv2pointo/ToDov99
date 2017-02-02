fb855a75308e9781d8d0a0d881a4a94cf916f293
isuddreth <iansuddreth@gmail.com>
2017-02-01 17:15:16 -0700

    Show categories in list details (#21)
    * ListToCategory relationship
    
    * Categories that belong to a List can be view in the details of the list
    
    * Changed Title to My Lists
    
    * Database connecct (#20)
    
    * add and view items
    
    * added IsComplete to CreateItem
    
    * Added partial views to display list and item tables
    
    * updated createItem so it now records list
    
    * added cat. contr
    
    * Lists can only see their own Items
    
    * added Text Box to create new items
    
    * Added Check Box to items which alters IsComplete in Item table
    
    * Added Database Creation Script
    
    * Added Database Folder
    
    * Updated Edit so it would keep track of ListID
    
    * Moved code for creation form of item into _ItemTable
    
    * moved the item table to Edit so we can get rid of ViewItems (#12)
    
    * databaseConnect
    
    * connected to local database
    
    * updated Database script
    
    * updating database cript
    
    * last commit. I think this one works best
    
    * update database name

--- ToDoListV99/Controllers/ListsController.cs
+++ ToDoListV99/Controllers/ListsController.cs
@@ -12,7 +12,10 @@ namespace ToDoListV99.Controllers
 {
     public class ListsController : Controller
     {
-        private MyDbContext db = new MyDbContext(); // alternate way would be to do a using (new MyDbContext) {}
+
+        private MyDbContext db = new MyDbContext();
+        protected static string CurrentListID { get; set; }
+
 
         // GET: Lists
         public ActionResult Index()
@@ -27,13 +30,24 @@ namespace ToDoListV99.Controllers
 
         public ActionResult BuildItemTable()
         {
-            return PartialView("_ItemTable", db.Items.ToList());
+            return PartialView("_ItemTable", GetMyItems());
+        }
+
+        public IEnumerable<Item> GetMyItems()
+        {
+            List currentList = db.Lists.FirstOrDefault
+                (x => x.ListId.ToString() == CurrentListID);
+            return db.Items.ToList().Where(x => x.List.ListId == currentList.ListId);
+
         }
 
         // GET: 
-        public ActionResult ViewItems()
+        public ActionResult ViewItems(int? id)
         {
-            return View(db.Items.ToList());
+            CurrentListID = id.ToString();
+            return View();
+            return PartialView("_ItemTable", db.Items.ToList());
+
         }
 
         // GET: Lists/Details/5
@@ -48,7 +62,33 @@ namespace ToDoListV99.Controllers
             {
                 return HttpNotFound();
             }
-            return View(list);
+            //return View(list);
+            var Results = from c in db.Categories
+                          select new
+                          {
+                              c.CategoryId,
+                              c.CategoryName,
+                              Checked = ((from ab in db.ListsToCategories
+                                          where (ab.ListId == id) & (ab.CategoryId == c.CategoryId)
+                                          select ab).Count() > 0)
+                          };
+
+            var MyViewModel = new ListsViewModel();
+
+            MyViewModel.ListId = id.Value;
+            MyViewModel.ListName = list.ListName;
+
+            var MyCheckBoxList = new List<CheckBoxViewModel>();
+
+            foreach (var item in Results)
+            {
+                MyCheckBoxList.Add(new CheckBoxViewModel { Id = item.CategoryId, Name = item.CategoryName, Checked = item.Checked });
+            }
+
+            MyViewModel.Categories = MyCheckBoxList;
+
+
+            return View(MyViewModel);
         }
 
         // GET: Lists/Create
@@ -77,37 +117,31 @@ namespace ToDoListV99.Controllers
 
 
 
-        // GET: Lists/Create
-        public ActionResult CreateItem()
-        {
-            return View();
-        }
+        
 
-        // POST: Lists/CreateItem
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult CreateItem([Bind(Include = "ItemID,Description,IsComplete")] Item item)
+        public ActionResult AJAXCreateItem([Bind(Include = "ItemID,Description")] Item item)
         {
             if (ModelState.IsValid)
             {
-                string currentListID = RouteData.Values["id"].ToString();
+
                 List currentList = db.Lists.FirstOrDefault
-                    (x => x.ListId.ToString() == currentListID);
+                    (x => x.ListId.ToString() == CurrentListID);
                 item.List = currentList;
+                item.IsComplete = false;
                 db.Items.Add(item);
                 db.SaveChanges();
-                return RedirectToAction("Index");
             }
 
-            return View(item);
+            return PartialView("_ItemTable", GetMyItems());
         }
 
 
         // GET: Lists/Edit/5
         public ActionResult Edit(int? id)
         {
+            CurrentListID = id.ToString();
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
@@ -117,18 +151,39 @@ namespace ToDoListV99.Controllers
             {
                 return HttpNotFound();
             }
-            /*****************************************************
-            add a category
-            *****************************************************/
-            //var Results = from c in db.Categories
-            //              select new
-            //              {
-            //                  c.CategoryId,
-            //                  c.CategoryName
-            //              };
-            //var MyCategories = new List<string>;
+
+
+            
 
             return View(list);
+
+           /* var Results = from c in db.Categories
+                          select new
+                          {
+                              c.CategoryId,
+                              c.CategoryName,
+                              Checked = ((from ab in db.ListsToCategories
+                                          where (ab.ListId == id) & (ab.CategoryId == c.CategoryId)
+                                          select ab).Count() > 0)
+                          };
+
+            var MyViewModel = new ListsViewModel();
+
+            MyViewModel.ListId = id.Value;
+            MyViewModel.ListName = list.ListName;
+
+            var MyCheckBoxList = new List<CheckBoxViewModel>();
+
+            foreach (var item in Results)
+            {
+                MyCheckBoxList.Add(new CheckBoxViewModel { Id = item.CategoryId, Name = item.CategoryName, Checked = item.Checked });
+            }
+
+            MyViewModel.Categories = MyCheckBoxList;
+
+
+            return View(MyViewModel);*/
+
         }
 
         // POST: Lists/Edit/5
@@ -136,17 +191,61 @@ namespace ToDoListV99.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ListId,ListName")] List list)
+        public ActionResult Edit(ListsViewModel list)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(list).State = EntityState.Modified;
+                var MyList = db.Lists.Find(list.ListId);
+
+                MyList.ListName = list.ListName;
+
+                foreach (var item in db.ListsToCategories)
+                {
+                    if (item.ListId == list.ListId)
+                    {
+                        db.Entry(item).State = System.Data.Entity.EntityState.Deleted;
+                    }
+                }
+
+                foreach (var item in list.Categories)
+                {
+                    if (item.Checked)
+                    {
+                        db.ListsToCategories.Add(new ListToCategory() { ListId = list.ListId, CategoryId = item.Id });
+                    }
+                }
+                //db.Entry(list).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
             return View(list);
         }
    
+
+        // POST: Lists/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        public ActionResult AJAXEditItem(int? id, bool value)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = db.Items.Find(id);
+            if (item == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                item.IsComplete = value;
+                db.Entry(item).State = EntityState.Modified;
+                db.SaveChanges();
+                return PartialView("_ItemTable", GetMyItems());
+            }
+        }
+
 
         // GET: Lists/Delete/5
         public ActionResult Delete(int? id)
