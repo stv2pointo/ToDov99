afc1eb236ac5a21e3155f00e186a955f9423cd84
Dax Kelson <daxkelson@gmail.com>
2017-02-03 14:29:59 -0700

    add support for Web Apis

---
+++ ToDoListV99/Controllers/api/ItemsController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ToDoListV99.Controllers.api
+{
+    public class ItemsController : ApiController
+    {
+    }
+}
