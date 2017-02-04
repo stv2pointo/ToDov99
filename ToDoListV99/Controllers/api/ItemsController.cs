5e7ae5c9390f9dba9e192d86b6a764b6759ac38d
Dax Kelson <daxkelson@gmail.com>
2017-02-03 18:25:06 -0700

    Add simple REST API for Items and Lists . items enumerate and item delete works, list enumerate works. The API could be fleshed out more.

--- ToDoListV99/Controllers/api/ItemsController.cs
+++ ToDoListV99/Controllers/api/ItemsController.cs
@@ -4,10 +4,36 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using ToDoListV99.Models;
 
 namespace ToDoListV99.Controllers.api
 {
     public class ItemsController : ApiController
     {
+        private MyDbContext _context;
+
+        public ItemsController()
+        {
+            _context = new MyDbContext();
+
+        }
+
+        public IHttpActionResult GetItems(string query = null)
+        {
+            return Ok(_context.Items.ToList());
+        }
+
+        public IHttpActionResult DeleteItem(int id)
+        {
+            var itemInDb = _context.Items.SingleOrDefault(c => c.ItemId == id);
+
+            if (itemInDb == null)
+                return NotFound();
+
+            _context.Items.Remove(itemInDb);
+            _context.SaveChanges();
+
+            return Ok();
+        }
     }
 }
