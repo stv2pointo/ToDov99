38c21e814437dbe6cf8a5e693834fa7d35762817
Morgan Dahl <morgancdahl@gmail.com>
2017-02-04 00:02:00 -0700

    Created deleteItem method in Services folder.  Lists can be deleted, items can be deleted, cleaned up a few labels.

---
+++ ToDoListV99/Services/ItemService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ToDoListV99.Models;
+
+namespace ToDoListV99.Services
+{
+    public class ItemService : IItemService
+    {
+        private MyDbContext _context = new MyDbContext();
+
+        public void deleteItem(int id)
+        {
+            var itemInDb = _context.Items.SingleOrDefault(c => c.ItemId == id);
+
+            _context.Items.Remove(itemInDb);
+
+            _context.SaveChanges();
+        }
+    }
+}
